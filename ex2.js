8f3172789cd59b6aee1ba174bbdd6b36370c6f19
Picabea <iniakipicabea@gmail.com>
2024-01-16 00:18:57 -0300

    Se eliminan archivos equivocos

--- ex2.js
+++
@@ -1,7 +0,0 @@
-img = document.querySelector("img")
-img.src = "https://4.bp.blogspot.com/-WQHuDy8L86I/TZCSKaHUftI/AAAAAAAACI8/pWhGzWTp2a8/s1600/Imagenes001__001079.jpg"
-
-function getRatio(){
-    
-}
-
